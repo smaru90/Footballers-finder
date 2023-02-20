fb26c10e90c99d684cf5abdb1d23194cacb26b1e
Mateusz Blaszczyk <121445191+smaru90@users.noreply.github.com>
2023-02-20 19:11:30 +0100

    Add files via upload

--- components/search-box/search-box.component.jsx
+++ components/search-box/search-box.component.jsx
@@ -1,111 +1,193 @@
 import { Component } from "react";
 
-import '../search-box/search-box.styles.css'
+import "../search-box/search-box.styles.css";
 
 class SearchBox extends Component {
-
-    render() {
-        return (  <div>  
-        <div>    
-        <input
-            className={ `search-box ${this.props.className}`}
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            className={`search-box ${this.props.className}`}
             type="search"
             placeholder={this.props.placeholder}
             onChange={this.props.onChangeHandler}
-        />
-        </div>  
+          />
+        </div>
         <div className="radio-boxes">
-        <fieldset className="radio-box">
+          <fieldset className="radio-box">
             <legend>Select Position:</legend>
             <p>
-            <input type="radio" id="goalkeeper" name="drone" value="goalkeeper"
-                    checked />
-            <label for="goalkeeper">Goalkeeper</label>
+              <input
+                type="radio"
+                id="goalkeeper"
+                name="player-card"
+                value="goalkeeper"
+                checked
+              />
+              <label for="goalkeeper">Goalkeeper</label>
             </p>
             <p>
-            <input type="radio" id="defender" name="drone" value="defender"/>
-            <label for="defender">Defender</label>
+              <input
+                type="radio"
+                id="defender"
+                name="player-card"
+                value="defender"
+              />
+              <label for="defender">Defender</label>
             </p>
             <p>
-            <input type="radio" id="midfielder" name="drone" value="midfielder"/>
-            <label for="midfielder">Midfielder</label>
+              <input
+                type="radio"
+                id="midfielder"
+                name="player-card"
+                value="midfielder"
+              />
+              <label for="midfielder">Midfielder</label>
             </p>
             <p>
-            <input type="radio" id="striker" name="drone" value="striker"/>
-            <label for="striker">Striker</label>
+              <input
+                type="radio"
+                id="striker"
+                name="player-card"
+                value="striker"
+              />
+              <label for="striker">Striker</label>
             </p>
-            
-        </fieldset>
-        <fieldset className="radio-box">
+          </fieldset>
+          <fieldset className="radio-box">
             <legend>Select nationality:</legend>
             <p>
-            <input type="radio" id="spain" name="drone" value="spain"
-                    checked />
-            <label for="spain">Spain</label>
+              <input
+                type="radio"
+                id="spain"
+                name="player-card"
+                value="spain"
+                checked
+              />
+              <label for="spain">Spain</label>
             </p>
             <p>
-            <input type="radio" id="portugal" name="drone" value="portugal"/>
-            <label for="portugal">Portugal</label>
+              <input
+                type="radio"
+                id="portugal"
+                name="player-card"
+                value="portugal"
+              />
+              <label for="portugal">Portugal</label>
             </p>
             <p>
-            <input type="radio" id="argentina" name="drone" value="argentina"/>
-            <label for="argentina">Argentina</label>
+              <input
+                type="radio"
+                id="argentina"
+                name="player-card"
+                value="argentina"
+              />
+              <label for="argentina">Argentina</label>
             </p>
             <p>
-            <input type="radio" id="brazil" name="drone" value="brazil"/>
-            <label for="brazil">Brazil</label>
+              <input
+                type="radio"
+                id="brazil"
+                name="player-card"
+                value="brazil"
+              />
+              <label for="brazil">Brazil</label>
             </p>
             <p>
-            <input type="radio" id="poland" name="drone" value="poland"/>
-            <label for="poland">Poland</label>
+              <input
+                type="radio"
+                id="poland"
+                name="player-card"
+                value="poland"
+              />
+              <label for="poland">Poland</label>
             </p>
-        </fieldset>
-        <fieldset className="radio-box">
+          </fieldset>
+          <fieldset className="radio-box">
             <legend>Age:</legend>
             <p>
-            <input type="radio" id="spain" name="drone" value="spain"
-                    checked />
-            <label for="spain">18-21</label>
+              <input
+                type="radio"
+                id="spain"
+                name="player-card"
+                value="spain"
+                checked
+              />
+              <label for="spain">18-21</label>
             </p>
             <p>
-            <input type="radio" id="portugal" name="drone" value="portugal"/>
-            <label for="portugal">22-25</label>
+              <input
+                type="radio"
+                id="portugal"
+                name="player-card"
+                value="portugal"
+              />
+              <label for="portugal">22-25</label>
             </p>
             <p>
-            <input type="radio" id="argentina" name="drone" value="argentina"/>
-            <label for="argentina">26-30</label>
+              <input
+                type="radio"
+                id="argentina"
+                name="player-card"
+                value="argentina"
+              />
+              <label for="argentina">26-30</label>
             </p>
             <p>
-            <input type="radio" id="brazil" name="drone" value="brazil"/>
-            <label for="brazil">30+</label>
-            </p>           
-        </fieldset>
-        <fieldset className="radio-box">
+              <input
+                type="radio"
+                id="brazil"
+                name="player-card"
+                value="brazil"
+              />
+              <label for="brazil">30+</label>
+            </p>
+          </fieldset>
+          <fieldset className="radio-box">
             <legend>Skill level:</legend>
             <p>
-            <input type="radio" id="spain" name="drone" value="spain"
-                    checked />
-            <label for="spain">60-70</label>
+              <input
+                type="radio"
+                id="spain"
+                name="player-card"
+                value="spain"
+                checked
+              />
+              <label for="spain">60-70</label>
             </p>
             <p>
-            <input type="radio" id="portugal" name="drone" value="portugal"/>
-            <label for="portugal">70-80</label>
+              <input
+                type="radio"
+                id="portugal"
+                name="player-card"
+                value="portugal"
+              />
+              <label for="portugal">70-80</label>
             </p>
             <p>
-            <input type="radio" id="argentina" name="drone" value="argentina"/>
-            <label for="argentina">80-90</label>
+              <input
+                type="radio"
+                id="argentina"
+                name="player-card"
+                value="argentina"
+              />
+              <label for="argentina">80-90</label>
             </p>
             <p>
-            <input type="radio" id="brazil" name="drone" value="brazil"/>
-            <label for="brazil">90+</label>
+              <input
+                type="radio"
+                id="brazil"
+                name="player-card"
+                value="brazil"
+              />
+              <label for="brazil">90+</label>
             </p>
-          
-        </fieldset>
+          </fieldset>
         </div>
-        </div>  
-        )
-
-    }
+      </div>
+    );
+  }
 }
 
 export default SearchBox;
