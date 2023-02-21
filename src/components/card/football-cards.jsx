534bb4da81ff681b14d72a366362f63161b2b54a
Mateusz Blaszczyk <121445191+smaru90@users.noreply.github.com>
2023-02-21 13:32:55 +0100

    Add files via upload

---
+++ src/components/card/football-cards.jsx
@@ -0,0 +1,189 @@
+import { Component } from "react";
+import "../card/card.styles.css";
+import "../card-list/card-list.styles.css";
+
+class Players extends Component {
+  render() {
+    return (
+      <div className="card-list">
+        <div className="card-container" key={1}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p158023.png?v=22"
+            alt="Messi"
+          />
+          <h2>Lionel Messi</h2>
+          <p>Argentina</p>
+          <p>Striker</p>
+          <p>Age: 35</p>
+          <p>Overall: 92</p>
+        </div>
+        <div className="card-container" key={2}>
+          <img
+            src="https://sortitoutsi.net/uploads/images/0ABH9RtBnMCjfFMocIaw5ghPrVoPYsqy.png"
+            alt="Lautaro Martinez"
+          />
+          <h2>Lautaro Martinez</h2>
+          <p>Argentina</p>
+          <p>Striker</p>
+          <p>Age: 24</p>
+          <p>Overall: 86</p>
+        </div>
+        <div className="card-container" key={3}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p247090.png?v=22"
+            alt="Enzo Fernandez"
+          />
+          <h2>Enzo Fernandez</h2>
+          <p>Argentina</p>
+          <p>Midfielder</p>
+          <p>Age: 21</p>
+          <p>Overall: 81</p>
+        </div>
+        <div className="card-container" key={4}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p212616.png?v=22"
+            alt="Rodrigo de Paul"
+          />
+          <h2>Rodrigo de Paul</h2>
+          <p>Argentina</p>
+          <p>Midfielder</p>
+          <p>Age: 28</p>
+          <p>Overall: 84</p>
+        </div>
+        <div className="card-container" key={5}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p251854.png?v=22"
+            alt="Pedri"
+          />
+          <h2>Pedri</h2>
+          <p>Spain</p>
+          <p>Midfielder</p>
+          <p>Age: 19</p>
+          <p>Overall: 84</p>
+        </div>
+        <div className="card-container" key={6}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p264240.png?v=22"
+            alt="Gavi"
+          />
+          <h2>Gavi</h2>
+          <p>Spain</p>
+          <p>Midfielder</p>
+          <p>Age: 17</p>
+          <p>Overall: 80</p>
+        </div>
+        <div className="card-container" key={7}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p188545.png?v=22"
+            alt="Robert Lewandowski"
+          />
+          <h2>Robert Lewandowski</h2>
+          <p>Poland</p>
+          <p>Striker</p>
+          <p>Age: 33</p>
+          <p>Overall: 91</p>
+        </div>
+        <div className="card-container" key={8}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p190547.png?v=22"
+            alt="Kamil Glik"
+          />
+          <h2>Kamil Glik</h2>
+          <p>Poland</p>
+          <p>Defender</p>
+          <p>Age: 34</p>
+          <p>Overall: 70</p>
+        </div>
+        <div className="card-container" key={9}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p186153.png?v=22"
+            alt="Wojciech Szczęsny"
+          />
+          <h2>Wojciech Szczęsny</h2>
+          <p>Poland</p>
+          <p>Goalkeeper</p>
+          <p>Age: 32</p>
+          <p>Overall: 86</p>
+        </div>
+        <div className="card-container" key={10}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p220570.png?v=22"
+            alt="Jan Bednarek"
+          />
+          <h2>Jan Bednarek</h2>
+          <p>Poland</p>
+          <p>Defender</p>
+          <p>Age: 26</p>
+          <p>Overall: 76</p>
+        </div>
+        <div className="card-container" key={11}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p242816.png?v=22"
+            alt="Ricard Puig"
+          />
+          <h2>Ricard Puig</h2>
+          <p>Spain</p>
+          <p>Midfielder</p>
+          <p>Age: 22</p>
+          <p>Overall: 75</p>
+        </div>
+        <div className="card-container" key={12}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p212831.png?v=22"
+            alt="Alisson Becker"
+          />
+          <h2>Alisson Becker</h2>
+          <p>Brazil</p>
+          <p>Goalkeeper</p>
+          <p>Age: 29</p>
+          <p>Overall: 89</p>
+        </div>
+        <div className="card-container" key={13}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p190871.png?v=22"
+            alt="Neymar"
+          />
+          <h2>Neymar</h2>
+          <p>Brazil</p>
+          <p>Striker</p>
+          <p>Age: 30</p>
+          <p>Overall: 89</p>
+        </div>
+        <div className="card-container" key={14}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p20801.png?v=22"
+            alt="Cristiano Ronaldo"
+          />
+          <h2>Cristiano Ronaldo</h2>
+          <p>Portugal</p>
+          <p>Striker</p>
+          <p>Age: 37</p>
+          <p>Overall: 88</p>
+        </div>
+        <div className="card-container" key={15}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p218667.png?v=22"
+            alt="Bernardo Silva"
+          />
+          <h2>Bernardo Silva</h2>
+          <p>Portugal</p>
+          <p>Midfielder</p>
+          <p>Age: 27</p>
+          <p>Overall: 88</p>
+        </div>
+        <div className="card-container" key={16}>
+          <img
+            src="https://cdn.fifacm.com/content/media/imgs/fifa23/players/p164240.png?v=22"
+            alt="Kamil Glik"
+          />
+          <h2>Thiago Silva</h2>
+          <p>Brazil</p>
+          <p>Defender</p>
+          <p>Age: 37</p>
+          <p>Overall: 86</p>
+        </div>
+      </div>
+    );
+  }
+}
+export default Players;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,20 +1,201 @@
 import { Component } from "react";
 
-import '../search-box/search-box.styles.css'
+import "../search-box/search-box.styles.css";
 
 class SearchBox extends Component {
-
-    render() {
-        return (        
-        <input
-            className={ `search-box ${this.props.className}`}
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            className={`search-box ${this.props.className}`}
             type="search"
             placeholder={this.props.placeholder}
             onChange={this.props.onChangeHandler}
-        />
-        )
+          />
+        </div>
+        <div className="radio-boxes">
+          <fieldset className="radio-box">
+            <legend>Select Position:</legend>
+            <p>
+              <input
+                type="radio"
+                id="goalkeeper"
+                name="player-position"
+                value="goalkeeper"
+                onChange={this.props.onChangeHandler}
+              />
+              <label for="goalkeeper">Goalkeeper</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="defender"
+                name="player-position"
+                value="defender"
+                onChange={this.props.onChangeHandler}
+              />
+              <label for="defender">Defender</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="midfielder"
+                name="player-position"
+                value="midfielder"
+                onChange={this.props.onChangeHandler}
+              />
+              <label for="midfielder">Midfielder</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="striker"
+                name="player-position"
+                value="striker"
+                onChange={this.props.onChangeHandler}
+              />
+              <label for="striker">Striker</label>
+            </p>
+          </fieldset>
+          <fieldset className="radio-box">
+            <legend>Select nationality:</legend>
+            <p>
+              <input
+                type="radio"
+                id="spain"
+                name="player-nationality"
+                value="spain"
+                checked
+                onChange={this.props.onChangeHandler}
 
-    }
+                
+              />
+              <label for="spain">Spain</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="portugal"
+                name="player-nationality"
+                value="portugal"
+                
+              />
+              <label for="portugal">Portugal</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="argentina"
+                name="player-nationality"
+                value="argentina"
+              />
+              <label for="argentina">Argentina</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="brazil"
+                name="player-nationality"
+                value="brazil"
+              />
+              <label for="brazil">Brazil</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="poland"
+                name="player-nationality"
+                value="poland"
+              />
+              <label for="poland">Poland</label>
+            </p>
+          </fieldset>
+          <fieldset className="radio-box">
+            <legend>Age:</legend>
+            <p>
+              <input
+                type="radio"
+                id="17-21"
+                name="player-age"
+                value="17-21"
+                checked
+              />
+              <label for="17-21">17-21</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="22-25"
+                name="player-age"
+                value="22-25"
+              />
+              <label for="22-25">22-25</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="26-30"
+                name="player-age"
+                value="26-30"
+              />
+              <label for="26-30">26-30</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="30+"
+                name="player-age"
+                value="30+"
+              />
+              <label for="30+">30+</label>
+            </p>
+          </fieldset>
+          <fieldset className="radio-box">
+            <legend>Skill level:</legend>
+            <p>
+              <input
+                type="radio"
+                id="61-70"
+                name="player-skill"
+                value="61-70"
+                checked
+              />
+              <label for="61-70">61-70</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="71-80"
+                name="player-skill"
+                value="71-80"
+              />
+              <label for="71-80">71-80</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="81-89"
+                name="player-skill"
+                value="81-89"
+              />
+              <label for="81-89">81-89</label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                id="90+"
+                name="player-skill"
+                value="90+"
+              />
+              <label for="90+">90+</label>
+            </p>
+          </fieldset>
+        </div>
+        <br/>
+      </div>
+    );
+  }
 }
 
 export default SearchBox;
